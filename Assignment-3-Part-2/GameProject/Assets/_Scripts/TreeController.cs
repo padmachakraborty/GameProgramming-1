ab0dd3dc658a6b5c3b38b8a33e26efe2bd3e28e5
padmachak1974 <padmachak1974@gmail.com>
2016-03-18 20:14:20 -0400

    Level-1 updated

---
+++ Assignment-3-Part-2/GameProject/Assets/_Scripts/TreeController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeController : MonoBehaviour {
+	private AudioSource[] _audioSources;
+	private AudioSource _waterSound;
+	// Use this for initialization
+	void Start () {
+		this._audioSources = gameObject.GetComponents<AudioSource> ();
+		this._waterSound = this._audioSources [1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Tree")) {
+			this._waterSound.Play ();
+		}
+
+	}
+}
